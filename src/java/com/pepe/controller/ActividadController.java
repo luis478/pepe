fc4759cd28747cedcf3cda8777fd1c516f709caa
Jhonny9022 <jhonny.agudelo@misena.edu.co>
2015-02-24 15:14:45 -0500

    actualizacion actividad y guia aprendizaje

--- src/java/com/pepe/controller/ActividadController.java
+++ src/java/com/pepe/controller/ActividadController.java
@@ -16,13 +16,12 @@ import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-
 import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
-import javax.inject.Named;
+
 
 
 
